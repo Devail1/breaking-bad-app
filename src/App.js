03e854886054fe292f8019f45efefe1ab0872332
Devail1 <liav.acc@gmail.com>
2022-01-09 19:55:20 +0200

    Created view "Main" component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+
+import { useEffect } from "react"
+import { useEpisodesFetch } from "./hooks/useEpisodesFetch"
+import Main from "./components/Main"
 
 function App() {
+  const { episodes, error, isLoading, fetchEpisodes } = useEpisodesFetch()
+
+  useEffect(() => {
+    fetchEpisodes()
+    //eslint-disable-next-line
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full  bg-gray-800">
+      <section className="max-w-6xl px-4 py-12 mx-auto sm:px-6 lg:px-4">
+        {error ? (
+          <> An Error has been occured. Please check your connection</>
+        ) : isLoading ? (
+          <>Loading data..</>
+        ) : (
+          <Main episodes={episodes} />
+        )}
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
