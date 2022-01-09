ad17119e33b99707c9d3fe23d991b422a490b63b
Devail1 <liav.acc@gmail.com>
2022-01-10 01:11:38 +0200

    added character route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import App from "./App"
 import Episode from "./views/Episode"
+import Character from "./views/Character"
 
 const rootElement = document.getElementById("root")
 
@@ -11,6 +12,7 @@ render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/episode/:episodeId" element={<Episode />} />
+      <Route path="/character/:characterId" element={<Character />} />
     </Routes>
   </BrowserRouter>,
   rootElement
